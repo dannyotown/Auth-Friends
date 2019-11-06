1d0cb1d512d28b8fb4dd0fad3a5e533941893144
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-11-05 20:10:46 -0500

    Update friends.js

--- friends/src/components/friends/friends.js
+++ friends/src/components/friends/friends.js
@@ -6,10 +6,10 @@ function Friends() {
   const [friends, setFriends] = useState([]);
   const [postFriend, setPostFriend] = useState({
     id: Date.now(),
-    name: '',
+    name: "",
     age: 0,
-    email: ''
-  })
+    email: ""
+  });
 
   useEffect(() => {
     api()
@@ -17,51 +17,53 @@ function Friends() {
       .then(result => setFriends(result.data))
       .catch(error => console.log(error));
   }, [postFriend]);
-  const onChange = e =>{
+  const onChange = e => {
     setPostFriend({
       ...postFriend,
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
   const onSubmit = e => {
     e.preventDefault();
     api()
-    .post('/api/friends', postFriend)
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
-    }
-  
+      .post("/api/friends", postFriend)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  };
+
   return (
     <>
+      <div style={{position:'relative', left:'25%'}}>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={postFriend.name}
+            name="name"
+            onChange={onChange}
+            placeholder="name"
+          />
+          <input
+            type="number"
+            value={postFriend.age}
+            name="age"
+            onChange={onChange}
+            placeholder="age"
+          />
+          <input
+            type="email"
+            value={postFriend.email}
+            name="email"
+            onChange={onChange}
+            placeholder="email"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
       <div className="CardBox">
         {friends.map((friend, index) => {
           return <FriendCards friend={friend} key={index} />;
         })}
       </div>
-      <form onSubmit={onSubmit}>
-        <input 
-        type="text"
-        value={postFriend.name}
-        name="name"
-        onChange={onChange}
-        placeholder="name"
-        />
-        <input 
-        type="number"
-        value={postFriend.age}
-        name="age"
-        onChange={onChange}
-        placeholder="age"
-        />
-        <input 
-        type="email"
-        value={postFriend.email}
-        name="email"
-        onChange={onChange}
-        placeholder="email"
-        />
-        <button type="submit">Submit</button>
-      </form>
     </>
   );
 }
